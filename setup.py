3adbfa8211710a9c1c5d5c1b69d5764182ea267d
Daniel Carrión <dani@indicalsoft.com>
2014-12-10 13:25:33 +0100

    version 0.0.1.dev2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_packages(package):
 
 setup(
     name='python-sepa',
-    version='0.0.1.dev1',
+    version='0.0.1.dev2',
     packages=get_packages('sepa'),
     install_requires=[
         'Jinja2',
